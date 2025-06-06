6cf6024181b6df4914b9943e018357737ab4b054
Muhammadamin-player <ibrohimovmuhammadamin888@gmail.com>
2025-06-06 16:54:26 +0500

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,40 @@
+// data type in js
+
+
+
+// number
+// string
+// boolean
+// simbool
+// undefined
+// null
+//  object
+//  bigint
+
+// data type 1 primative
+
+// data type not primative  object array function
+
+
+
+
+console.log(1231234);/* number */
+console.log('Odil'); /* string */
+console.log(1 != 2);
+const id1 = Symbol("userID");
+const diyorRaxmatullaev = Symbol("userID");
+
+console.log(id1 === id2); // false — ikkalasi ham noyob
+console.log(namee);
+var namee = 'dddd'
+
+console.log(typeof({}));
+
+
+
+let name = prompt('ismizi soravoman')
+let age = +prompt('yoshiziyam yozvorin oka')
+
+console.log(name + age);
+console.log(typeof(name)  +  typeof(age) );
+console.log('type name '+  Boolean(name) );
